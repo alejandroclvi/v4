82bcd6a50523b4a91e7523ea0a835b05f246cdad
mcalvinobrandcoders <84166590+mcalvinobrandcoders@users.noreply.github.com>
2023-10-19 07:17:51 -0400

    fixes

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,103 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
-import anime from 'animejs';
-import styled from 'styled-components';
-import { IconLoader } from '@components/icons';
+import styled, { ThemeProvider } from 'styled-components';
+import { Head, Loader, Nav, Social, Email, Footer } from '@components';
+import { GlobalStyle, theme } from '@styles';
 
-const StyledLoader = styled.div`
-  ${({ theme }) => theme.mixins.flexCenter};
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  background-color: var(--dark-navy);
-  z-index: 99;
-
-  .logo-wrapper {
-    width: max-content;
-    max-width: 100px;
-    transition: var(--transition);
-    opacity: ${props => (props.isMounted ? 1 : 0)};
-    svg {
-      display: block;
-      width: 100%;
-      height: 100%;
-      margin: 0 auto;
-      fill: none;
-      user-select: none;
-      #B {
-        opacity: 0;
-      }
-    }
-  }
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
 
-const Loader = ({ finishLoading }) => {
-  const [isMounted, setIsMounted] = useState(false);
+const Layout = ({ children, location }) => {
+  const isHome = location.pathname === '/';
+  const [isLoading, setIsLoading] = useState(isHome);
 
-  const animate = () => {
-    const loader = anime.timeline({
-      complete: () => finishLoading(),
-    });
-
-    loader
-      .add({
-        targets: '#logo path',
-        delay: 300,
-        duration: 1500,
-        easing: 'easeInOutQuart',
-        strokeDashoffset: [anime.setDashoffset, 0],
-      })
-      .add({
-        targets: '#logo #B',
-        duration: 700,
-        easing: 'easeInOutQuart',
-        opacity: 1,
-      })
-      .add({
-        targets: '#logo',
-        delay: 500,
-        duration: 300,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        scale: 0.1,
-      })
-      .add({
-        targets: '.loader',
-        duration: 200,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        zIndex: -1,
+  // Sets target="_blank" rel="noopener noreferrer" on external links
+  const handleExternalLinks = () => {
+    const allLinks = Array.from(document.querySelectorAll('a'));
+    if (allLinks.length > 0) {
+      allLinks.forEach(link => {
+        if (link.host !== window.location.host) {
+          link.setAttribute('rel', 'noopener noreferrer');
+          link.setAttribute('target', '_blank');
+        }
       });
+    }
   };
 
   useEffect(() => {
-    const timeout = setTimeout(() => setIsMounted(true), 10);
-    setIsMounted(true)
-    const t2 = setTimeout(() => animate(), 1000)
-    return () => {
-      () => clearTimeout(timeout);
-      () => clearTimeout(t2)
+    if (isLoading) {
+      return;
     }
-  }, [isMounted]);
+
+    if (location.hash) {
+      const id = location.hash.substring(1); // location.hash without the '#'
+      setTimeout(() => {
+        const el = document.getElementById(id);
+        if (el) {
+          el.scrollIntoView();
+          el.focus();
+        }
+      }, 0);
+    }
+
+    handleExternalLinks();
+  }, [isLoading]);
 
   return (
-    <StyledLoader className="loader" isMounted={isMounted}>
-      <Helmet bodyAttributes={{ class: `hidden` }} />
+    <>
+      <Head />
 
-      <div className="logo-wrapper">
-        <IconLoader />
+      <div id="root">
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+
+          <a className="skip-to-content" href="#content">
+            Skip to Content
+          </a>
+
+          {isLoading && isHome ? (
+            <Loader finishLoading={() => setIsLoading(false)} />
+          ) : (
+            <StyledContent>
+              <Nav isHome={isHome} />
+              <Social isHome={isHome} />
+              <Email isHome={isHome} />
+
+              <div id="content">
+                {children}
+                <Footer />
+              </div>
+            </StyledContent>
+          )}
+        </ThemeProvider>
       </div>
-    </StyledLoader>
+    </>
   );
 };
 
-Loader.propTypes = {
-  finishLoading: PropTypes.func.isRequired,
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.object.isRequired,
 };
 
-export default Loader;
+export default Layout;
